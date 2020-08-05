2dd28469b32f45c54191588fae96edc8d139d98e
raagz123 <aluri.venkatraghavender12@gmail.com>
2020-08-05 22:01:12 +1000

    Update testserver.js

--- public/testserver.js
+++ public/testserver.js
@@ -1,21 +1,24 @@
-const express=require('express')
-let app=express()
-app.use(express.static(__dirname+ '/public'));
 
-let returDeposit=function(name){
-    // let​​ accounts​=[
-    //     {​id:​1​,​name:​'alex'​,​deposit:​5​},
-    //     {​id:​2​,​name:​'sarah'​,​deposit:​5​},
-    //     {​id:​3​,​name:​'jim'​,​deposit:​15​}
-    // ];
-    let deposit=accounts[name];
-    return deposit;
-}
+//just ignore this file, its meant for testing purpose
 
-app.get('/accounts',function(request,response){
-    let id=(request.query.name);
-    let deposit=returDeposit(name);
-    response.send('Given name:'+name,'Deposit:'+deposit);
+// const express=require('express')
+// let app=express()
+// app.use(express.static(__dirname+ '/public'));
 
-})
-app.listen(80)
+// let returDeposit=function(name){
+//     // let​​ accounts​=[
+//     //     {​id:​1​,​name:​'alex'​,​deposit:​5​},
+//     //     {​id:​2​,​name:​'sarah'​,​deposit:​5​},
+//     //     {​id:​3​,​name:​'jim'​,​deposit:​15​}
+//     // ];
+//     let deposit=accounts[name];
+//     return deposit;
+// }
+
+// app.get('/accounts',function(request,response){
+//     let id=(request.query.name);
+//     let deposit=returDeposit(name);
+//     response.send('Given name:'+name,'Deposit:'+deposit);
+
+// })
+// app.listen(80)
